9839ac827b6256988def737c593a52192502e18e
halljc76 <jchall076@outlook.com>
2023-03-05 21:12:26 -0500

    Found way to unpack data -- download from repo first

---
+++ getData.R
@@ -0,0 +1,8 @@
+# Set the file path to the .zip file -- use the absolute path from your downloads folder
+# Make sure working directory is the 'ProjWebsite' 
+
+# Change the filepath to whatever file you are unzipping
+file_path <- "C:/Users/Carter/Downloads/subjects_0-1999_72_imgs.zip"
+
+# Run this -- takes very long time
+unzip(file_path,exdir = "./data/subjects_0-1999_72_imgs")
